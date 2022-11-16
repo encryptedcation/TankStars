e39d26d72873e6bc08ac4956057c4cd93e2dd0e2
sociallyencrypted <mehularora810@gmail.com>
2022-11-17 01:08:22 +0530

    initial commit

---
+++ core/src/com/encryptedcation/tankstars/TankStars.java
@@ -0,0 +1,31 @@
+package com.encryptedcation.tankstars;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class TankStars extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("Sprite/Button.png");
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(0, 0, 0, 0);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
