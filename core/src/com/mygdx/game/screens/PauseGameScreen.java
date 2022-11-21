3aceaf58b7b56c3698a06520f1f7cf203c93063a
Ananya Goyal <78123938+cation03@users.noreply.github.com>
2022-11-22 02:19:01 +0530

    added PauseGameScreen

--- core/src/com/mygdx/game/screens/PauseGameScreen.java
+++ core/src/com/mygdx/game/screens/PauseGameScreen.java
@@ -10,12 +10,29 @@ public class PauseGameScreen implements Screen {
     TankStars game;
     private Stage stage;
     private Texture bg;
+    private Texture resume;
+    private Texture resumeSel;
+    private Texture mainMenu;
+    private Texture mainMenuSel;
+    private Texture saveGame;
+    private Texture saveGameSel;
+    private Texture restart;
+    private Texture restartSel;
+
 
 
     public PauseGameScreen(TankStars game) {
         this.game = game;
         stage = new Stage(new StretchViewport(1920, 1080));
-        bg = new Texture("nightBG.png");
+        bg = new Texture("BG.png");
+        resume = new Texture("RESUME.png");
+        resumeSel = new Texture("RESUMEACTIVE.png");
+        mainMenu = new Texture("MAINMENU.png");
+        mainMenuSel = new Texture("MAINMENUACTIVE.png");
+        saveGame = new Texture("SAVEGAME.png");
+        saveGameSel = new Texture("SAVEGAMEACTIVE.png");
+        restart = new Texture("RESTART.png");
+        restartSel = new Texture("RESTARTACTIVE.png");
     }
 
     @Override
@@ -29,8 +46,11 @@ public class PauseGameScreen implements Screen {
         stage.draw();
         stage.getBatch().begin();
         stage.getBatch().draw(bg, 0, 0);
+        stage.getBatch().draw(resume, 793, 754);
+        stage.getBatch().draw(mainMenu, 793, 578);
+        stage.getBatch().draw(saveGame, 793, 403);
+        stage.getBatch().draw(restart, 793, 226);
         stage.getBatch().end();
-
     }
 
     @Override
