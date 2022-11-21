24550f796fc8ec86b1d65400a0683471299b6ad5
Ananya Goyal <78123938+cation03@users.noreply.github.com>
2022-11-21 13:36:47 +0530

    initialCommit

---
+++ core/src/com/mygdx/game/TankStars.java
@@ -0,0 +1,32 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.mygdx.game.screens.MainScreen;
+
+public class TankStars extends Game {
+	public SpriteBatch batch;
+	public static int WIDTH = 1920;
+	public static int HEIGHT = 1080;
+//	private Stage stage;
+
+	@Override
+	public void create () {
+//		stage = new Stage(new StretchViewport(2688, 1242));
+		batch = new SpriteBatch();
+		this.setScreen(new MainScreen(this));
+	}
+
+	@Override
+	public void render () {
+		super.render();
+//		stage = new Stage(new StretchViewport(2688, 1242));
+	}
+}
