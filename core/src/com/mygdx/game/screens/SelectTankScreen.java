6256761325b1bc17940d796b7c88d7fdd504b76b
sociallyencrypted <mehularora810@gmail.com>
2022-11-25 23:07:49 +0530

    SelectTankScreen edits done yayyyy

--- core/src/com/mygdx/game/screens/SelectTankScreen.java
+++ core/src/com/mygdx/game/screens/SelectTankScreen.java
@@ -1,5 +1,6 @@
 package com.mygdx.game.screens;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
@@ -49,12 +50,15 @@ public class SelectTankScreen implements Screen {
     private Texture highlightedMark;
     private Texture highlightedBlazer;
     private Texture highlightedToxic;
+    private Texture playerOneChooseActive;
+    private Texture playerTwoChooseActive;
 
     private Texture chooseSel;
     private int CHOOSE_BUTTON_WIDTH = 319;
     private int CHOOSE_BUTTON_HEIGHT = 146;
     private int CHOOSE_BUTTON_Y = 222;
     private int flag = 0;
+    private int flagPlayer = 0;
     public SelectTankScreen(final TankStars game) {
         this.game = game;
 
@@ -76,6 +80,8 @@ public class SelectTankScreen implements Screen {
         highlightedBlazer = new Texture("hltdBlazer.png");
         highlightedMark = new Texture("highlightedMark.png");
         highlightedToxic = new Texture("hltdToxic.png");
+        playerOneChooseActive = new Texture("playerOneChooseActive.png");
+        playerTwoChooseActive = new Texture("playerTwoChooseActive.png");
         names = new Texture("names.png");
         stage = new Stage(new StretchViewport(1920, 1080));
     }
@@ -91,8 +97,15 @@ public class SelectTankScreen implements Screen {
         ScreenUtils.clear(0, 0, 0, 1);
         stage.getBatch().begin();
         stage.getBatch().draw(img, x, y);
-        stage.getBatch().draw(playerOneChoose, 359, 926);
-        stage.getBatch().draw(playerTwoChoose, 1201, 926);
+        // if flagPlayer == 0, player 1 is choosing
+        // if flagPlayer == 1, player 2 is choosing
+        if (flagPlayer == 0) {
+            stage.getBatch().draw(playerOneChooseActive, 359, 926);
+            stage.getBatch().draw(playerTwoChoose, 1201, 926);
+        } else {
+            stage.getBatch().draw(playerOneChoose, 359, 926);
+            stage.getBatch().draw(playerTwoChooseActive, 1201, 926);
+        }
         stage.getBatch().draw(circleNoBG, 147, 657);
         stage.getBatch().draw(circleNoBG, 431, 657);
         stage.getBatch().draw(circleNoBG, 715, 657);
@@ -150,6 +163,49 @@ public class SelectTankScreen implements Screen {
             }
         }
 
+        // if clicked, save highlight
+        if (Gdx.input.isTouched()) {
+            if (Gdx.input.getX() > 147 && Gdx.input.getX() < 147 + 256 && Gdx.input.getY() > 1080 - 657 - 256 && Gdx.input.getY() < 1080 - 657) {
+                flag = 1;
+            }
+            if (Gdx.input.getX() > 431 && Gdx.input.getX() < 431 + 256 && Gdx.input.getY() > 1080 - 657 - 256 && Gdx.input.getY() < 1080 - 657) {
+                flag = 2;
+            }
+            if (Gdx.input.getX() > 715 && Gdx.input.getX() < 715 + 256 && Gdx.input.getY() > 1080 - 657 - 256 && Gdx.input.getY() < 1080 - 657) {
+                flag = 3;
+            }
+            if (Gdx.input.getX() > 975 && Gdx.input.getX() < 975 + 256 && Gdx.input.getY() > 1080 - 657 - 256 && Gdx.input.getY() < 1080 - 657) {
+                flag = 4;
+            }
+            if (Gdx.input.getX() > 1259 && Gdx.input.getX() < 1259 + 256 && Gdx.input.getY() > 1080 - 657 - 256 && Gdx.input.getY() < 1080 - 657) {
+                flag = 5;
+            }
+            if (Gdx.input.getX() > 1543 && Gdx.input.getX() < 1543 + 256 && Gdx.input.getY() > 1080 - 657 - 256 && Gdx.input.getY() < 1080 - 657) {
+                flag = 6;
+            }
+        }
+        // if flag is set, draw the highlighted image
+        if (flag == 1) {
+            stage.getBatch().draw(highlightedMark, 147, 657);
+        }
+        if (flag == 2) {
+            stage.getBatch().draw(highlightedToxic, 431, 657);
+        }
+        if (flag == 3) {
+            stage.getBatch().draw(highlightedBlazer, 715, 657);
+        }
+        if (flag == 4) {
+            stage.getBatch().draw(highlightedMark, 975, 657);
+        }
+        if (flag == 5) {
+            stage.getBatch().draw(highlightedToxic, 1259, 657);
+        }
+        if (flag == 6) {
+            stage.getBatch().draw(highlightedBlazer, 1543, 657);
+        }
+
+
+
 
 
 
@@ -161,12 +217,17 @@ public class SelectTankScreen implements Screen {
         if (Gdx.input.getX() < x + CHOOSE_BUTTON_WIDTH && Gdx.input.getX() > x && TankStars.HEIGHT - Gdx.input.getY() < CHOOSE_BUTTON_Y + CHOOSE_BUTTON_HEIGHT && TankStars.HEIGHT - Gdx.input.getY() > CHOOSE_BUTTON_Y) {
             stage.getBatch().draw(chooseSel, x, CHOOSE_BUTTON_Y, CHOOSE_BUTTON_WIDTH, CHOOSE_BUTTON_HEIGHT);
             if (Gdx.input.justTouched()) {
+                if (flagPlayer == 0) {
+                    flagPlayer = 1;
+                    flag = 0;
+                } else if (flagPlayer == 1) {
+                    // move to game screen
                     game.setScreen(new InGameScreen(game));
-                    this.dispose();
                 }
             }
-        else{
-            stage.getBatch().draw(choose,x, CHOOSE_BUTTON_Y, CHOOSE_BUTTON_WIDTH, CHOOSE_BUTTON_HEIGHT);
+        }
+        else {
+            stage.getBatch().draw(choose, x, CHOOSE_BUTTON_Y, CHOOSE_BUTTON_WIDTH, CHOOSE_BUTTON_HEIGHT);
         }
         stage.getBatch().end();
     }
