32c4b4379e1b4f866db6be5e59bbb8c4915fa2e2
sociallyencrypted <mehularora810@gmail.com>
2022-12-16 17:02:49 +0530

    added box2d support finally T_T

---
+++ core/src/com/encryptedcation/tankstars/screens/AltInGameScreen.java
@@ -0,0 +1,57 @@
+package com.encryptedcation.tankstars.screens;
+
+//note to ananya: making new screen for experimentation so that we don't mess up the original code
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.encryptedcation.tankstars.TankStars;
+
+//add box2d
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class AltInGameScreen implements Screen {
+    World world;
+    Box2DDebugRenderer b2dr;
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
