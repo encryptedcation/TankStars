abb254037e02e6a4b05939ff31195c50476c32d3
sociallyencrypted <mehularora810@gmail.com>
2022-12-20 02:53:33 +0530

    Added HUD, works properly but no map showing???

--- core/src/com/encryptedcation/tankstars/screens/AltInGameScreen.java
+++ core/src/com/encryptedcation/tankstars/screens/AltInGameScreen.java
@@ -3,19 +3,32 @@ package com.encryptedcation.tankstars.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.encryptedcation.tankstars.TankStars;
+import com.encryptedcation.tankstars.scenes.gameHUD;
 
 public class AltInGameScreen implements Screen {
     private final TankStars game;
     private final OrthographicCamera tankCam;
     private final Viewport tankPort;
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+    private gameHUD hud;
 
     public AltInGameScreen(TankStars game) {
         this.game = game;
         tankCam = new OrthographicCamera();
         tankPort = new FitViewport(TankStars.WIDTH, TankStars.HEIGHT, tankCam);
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("map.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        tankCam.position.set(tankPort.getWorldWidth(), tankPort.getWorldHeight(), 0);
+        hud = new gameHUD(game.batch);
     }
 
     @Override
@@ -25,7 +38,11 @@ public class AltInGameScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        game.batch.setProjectionMatrix(tankCam.combined);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(Gdx.gl.GL_COLOR_BUFFER_BIT);
+        renderer.render();
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
     }
 
     @Override
