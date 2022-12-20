7e1bb10e5e4a9b96d1906986c398c3527f4f1932
sociallyencrypted <mehularora810@gmail.com>
2022-12-20 10:14:01 +0530

    added box2d + created polygons on map + box2d rendering

--- core/src/com/encryptedcation/tankstars/screens/AltInGameScreen.java
+++ core/src/com/encryptedcation/tankstars/screens/AltInGameScreen.java
@@ -3,14 +3,24 @@ package com.encryptedcation.tankstars.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.PolygonMapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.EarClippingTriangulator;
+import com.badlogic.gdx.math.Polygon;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.encryptedcation.tankstars.Player;
 import com.encryptedcation.tankstars.TankStars;
 import com.encryptedcation.tankstars.scenes.gameHUD;
+
+import java.util.ArrayList;
 
 public class AltInGameScreen implements Screen {
     private final TankStars game;
@@ -21,6 +31,10 @@ public class AltInGameScreen implements Screen {
     private OrthogonalTiledMapRenderer renderer;
     private gameHUD hud;
 
+    // box2D variables
+    private World world;
+    private Box2DDebugRenderer b2dr;
+
     public AltInGameScreen(TankStars game) {
         this.game = game;
         tankCam = new OrthographicCamera();
@@ -30,6 +44,23 @@ public class AltInGameScreen implements Screen {
         renderer = new OrthogonalTiledMapRenderer(map);
         tankCam.position.set(tankPort.getWorldWidth()/2, tankPort.getWorldHeight()/2, 0);
         hud = new gameHUD(game.batch);
+
+        world = new World(new Vector2(0, -10), true);
+        b2dr = new Box2DDebugRenderer();
+
+        BodyDef bdef = new BodyDef();
+        PolygonShape shape = new PolygonShape();
+        Body body;
+        FixtureDef fdef = new FixtureDef();
+        for (MapObject object : map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set(rect.getX() + rect.getWidth()/2, rect.getY() + rect.getHeight()/2);
+            body = world.createBody(bdef);
+            shape.setAsBox(rect.getWidth()/2, rect.getHeight()/2);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
     }
 
     @Override
@@ -44,6 +75,7 @@ public class AltInGameScreen implements Screen {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(Gdx.gl.GL_COLOR_BUFFER_BIT);
         renderer.render();
+        b2dr.render(world, tankCam.combined);
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
     }
